19b0eff4077dbda5d4ae8cf97ae0cc9e153989d0
jwong65 <jason.wong6507@gmail.com>
2023-04-18 13:56:09 -0400

    Added the string return

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class Currency:
   #add magic methods here
   def __repr__(self):
   # This method returns the string to be printed. This should be the value rounded to two digits, accompanied by its acronym.
-    pass
+    return (str(round(self.value, 2)) + self.unit)
   
   def __str__(self):
     #This method returns the same value as __repr__(self).
@@ -35,10 +35,11 @@ class Currency:
   
 
 v1 = Currency(23.43, "EUR")
-v2 = Currency(19.97, "USD")
-print(v1 + v2)
-print(v2 + v1)
-print(v1 + 3) # an int or a float is considered to be a USD value
-print(3 + v1)
-print(v1 - 3) # an int or a float is considered to be a USD value
-print(30 - v2) 
+print(v1)
+# v2 = Currency(19.97, "USD")
+# print(v1 + v2)
+# print(v2 + v1)
+# print(v1 + 3) # an int or a float is considered to be a USD value
+# print(3 + v1)
+# print(v1 - 3) # an int or a float is considered to be a USD value
+# print(30 - v2) 
